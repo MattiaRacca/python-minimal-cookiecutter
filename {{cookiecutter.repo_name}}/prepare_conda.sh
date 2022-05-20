3ed62f55d2f119e4207c76f5e49ab782d48e8863
MattiaRacca <mattia.rh@gmail.com>
2022-05-20 12:41:17 +0200

    revert some conda automation

--- {{cookiecutter.repo_name}}/prepare_conda.sh
+++ {{cookiecutter.repo_name}}/prepare_conda.sh
@@ -1,5 +1,2 @@
 conda create --name {{cookiecutter.conda_env_name}} python={{cookiecutter.python_version}}
-conda activate {{cookiecutter.conda_env_name}}
-conda env export > environment.yml
-conda deactivate
 conda env list
